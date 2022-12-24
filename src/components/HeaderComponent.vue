1cc0c5db5ab06807af11b91b0b56ddf52837cdc1
Matheus Alves <matheusfrancisco0330@gmail.com>
2022-12-24 10:58:05 -0300

    feat: two-way data binding

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -6,6 +6,29 @@
             <li :class="{'active': about}">About</li>
         </ul>
     </nav>
+    <div>
+        <input type="text" name="name" id="name" v-model="name">
+        <br>
+        <select v-model="esporte">
+            <option value="">Escolha</option>
+            <option value="futebol">futebol</option>
+            <option value="vôlei">vôlei</option>
+            <option value="basquete">basquete</option>
+        </select>
+        <br>
+        <label>Quer receber nossas notificações?</label>
+        <input type="radio" v-model="notificações" value="sim">sim
+        <input type="radio" v-model="notificações" value="não">não
+        <br>
+        <label>Aceita nossos termos?</label>
+        <input type="checkbox" v-model="termos">
+        <br>
+        <input type="checkbox" v-model="preferencias" value="Azul">Azul
+        <input type="checkbox" v-model="preferencias" value="Verde">Verde
+        <input type="checkbox" v-model="preferencias" value="Amarelo">Amarelo
+        <input type="checkbox" v-model="preferencias" value="Vermelho">Vermelho
+        <p>{{ name }} | {{ esporte }} | Receber notificações? {{ notificações }} | Aceita nossos termos? {{ termos }} | {{ preferencias }}</p>
+    </div>
 </template>
 
 <script>
@@ -15,7 +38,12 @@
             return {
                 home: false,
                 services: true,
-                about: false
+                about: false,
+                name: "Harry Potter",
+                esporte: '',
+                notificações: '',
+                termos: '',
+                preferencias: [],
             }
         }
     }
