ef9feaff7b68141789aa459d8a37cac1df6dbca0
Matheus Alves <matheusfrancisco0330@gmail.com>
2022-12-24 10:13:38 -0300

    feat: css dinâmico

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,11 +1,11 @@
 <template>
-    <h1 class="header">Hello world</h1>
-    <div>
-        <div class="todo-list" v-for="todo in todos" :key="todo.id">
-            {{ todo.title }}
-            <img :src="todo.photo" :alt="todo.title">
-        </div>
-    </div>
+    <nav>
+        <ul>
+            <li :class="{'active': home}">Home</li>
+            <li :class="{'active': services}">Services</li>
+            <li :class="{'active': about}">About</li>
+        </ul>
+    </nav>
 </template>
 
 <script>
@@ -13,44 +13,27 @@
         name: 'HeaderComponent',
         data(){
             return {
-                todos: [
-                    {
-                        id: 1,
-                        title: 'Título I',
-                        photo: 'https://via.placeholder.com/150'
-                    },
-                    {
-                        id: 2,
-                        title: 'Título II',
-                        photo: 'https://via.placeholder.com/150'
-                    },
-                    {
-                        id: 3,
-                        title: 'Título III',
-                        photo: 'https://via.placeholder.com/150'
-                    },
-                    {
-                        id: 4,
-                        title: 'Título IV',
-                        photo: 'https://via.placeholder.com/150'
-                    },
-                ]
+                home: false,
+                services: true,
+                about: false
             }
         }
     }
 </script>
 
 <style>
-    .header{
-        background: #7787;
-        color: #f8f8f8;
+    ul{
+        display: flex;
+        align-items: center;
+        justify-content: center;
     }
-    .todo-list{
-        align-items: flex-start;
-        background: #7787;
-        color: #f8f8f8;
-        padding: 3px 6px;
-        margin: 4px 0 0 0;
-        border-radius: 12px;
+    li{
+        list-style-type: none;
+        margin: 6px 12px;
+        font-weight: 600;
+        cursor: pointer;
+    }
+    .active{
+        color:crimson
     }
 </style>
