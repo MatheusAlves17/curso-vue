4a13e139fbeaff5720a2c542600a1da9d89b3f2f
Matheus Alves <matheusfrancisco0330@gmail.com>
2022-12-24 09:10:24 -0300

    feat: componentes de arquivo único

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1 class="header">Hello world</h1>
+</template>
+
+<script>
+    
+</script>
+
+<style>
+    .header{
+        background: #7787;
+        color: #f8f8f8;
+    }
+</style>
